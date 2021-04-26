07d60ef0e9718bee2459881d77e79897bae9761f
Unknown <unknown@example.com>
2021-04-26 18:58:42 +0430

    added passVerifier code

--- app/main.cpp
+++ app/main.cpp
@@ -3,10 +3,75 @@
 #include <stdlib.h>
 
 #include "Foo.h"
+
+int count{};
+
+bool numch(char* ch)
+{
+  int check{};
+  for (int i = 0; i < count; i++)
+  {
+    for (char j = 48; j <= 57; j++)
+    {
+      if (ch[i] == j)
+      {
+        check = 1;
+        break;
+      }
+    }
+  }
+  return check;
+}
+
+bool uppercase(char* ch)
+{
+  int check{};
+  for (int i = 0; i < count; i++)
+  {
+    for (char j = 65; j <= 90; j++)
+    {
+      if (ch[i] == j)
+      {
+        check = 1;
+        break;
+      }
+    }
+  }
+  return check;
+}
+
+bool lowercase(char* ch)
+{
+  int check{};
+  for (int i = 0; i < count; i++)
+  {
+    for (char j = 97; j <= 122; j++)
+    {
+      if (ch[i] == j)
+      {
+        check = 1;
+        break;
+      }
+    }
+  }
+  return check;
+}
+
+bool passVerifier(char* pass)
+{
+  bool check = (count >=6 && uppercase(pass) && lowercase(pass) && numch(pass));
+  return check;
+}
+
 int main()
 {
-  Foo d = Foo();
-  std::cout<< d.doSomething()<<std::endl;
+  char pass[120] = {};
+  std::cout << "Enter pass : ";
+  std::cin >> pass;
+
+  for (count = 0; pass[count] != 0; count++);
+
+  std::cout << '(' << pass << ") ---> " << (passVerifier(pass) ? "true" : "false") << '\n';
 
   return 0;
 }
---
+++ build/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/mahdi/github/Eaglet/passVerifier/tests
+# Build directory: /home/mahdi/github/Eaglet/passVerifier/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(myLibrary.unit_tests "/home/mahdi/github/Eaglet/passVerifier/build/unit_tests")
+set_tests_properties(myLibrary.unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/mahdi/github/Eaglet/passVerifier/tests/CMakeLists.txt;22;add_test;/home/mahdi/github/Eaglet/passVerifier/tests/CMakeLists.txt;0;")
